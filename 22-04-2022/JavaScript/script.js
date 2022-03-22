4ba53aacc8caae416c635a88ca5047c3dfc62438
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-03-22 14:55:59 +0600

    𝕁𝕊-1 Variables and Constants

--- 22-04-2022/JavaScript/script.js
+++ 22-04-2022/JavaScript/script.js
@@ -1,1 +1,43 @@
-console.log("Test");
+//console.log("Test");
+/*
+let test = 42;
+console.log(test);
+*/
+/*
+let string = "String",
+  num = 22,
+  fun = true;
+
+console.log(string, num, fun);
+*/
+/*
+let empty;
+
+console.log(empty);
+*/
+/*
+let phase1 = 10;
+
+phase1 = 55;
+
+console.log(phase1);
+*/
+/*
+let empty;
+
+empty = 100;
+
+console.log(empty);
+*/
+/*
+const test = 22;
+test = 44;
+console.log(test);
+*/
+/*
+const empty;
+
+console.log(empty);
+*/
+console.log(access);
+var access = 65;
