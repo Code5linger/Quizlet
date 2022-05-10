1de0ecd9ca6e06263499723e1c748020b88644e4
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-05-10 21:23:19 +0600

    w

--- 2022-05-10/script.js
+++ 2022-05-10/script.js
@@ -41,6 +41,73 @@ console.log(solid);
 
 const test2 = 99;
 console.log(test2);
-
+/*
 console.log(e);
 const e = 11;
+*/
+let num1 = 17;
+console.log(typeof num1);
+
+let d = 1,
+  e = "x ",
+  f = true;
+
+console.log(typeof a, typeof b, typeof c);
+
+let g = 1;
+console.log(g + 1);
+let h = 1;
+console.log(g - 1);
+let i = 5;
+console.log(i * 5);
+let j = 55;
+console.log(j / 5);
+let k = 51;
+console.log(k % 5);
+
+let l = 1;
+console.log(++l);
+let m = 2;
+console.log(--m);
+console.log(m--);
+
+let n = 5;
+console.log(n * 5);
+let o = 25;
+console.log(o / 5);
+
+let p = 29;
+console.log(p % 3);
+
+let q = 5,
+  r = 7;
+console.log(q * r);
+
+let s = 89;
+console.log(++s);
+
+let t = 100;
+t--;
+console.log(t--);
+console.log(--t);
+
+let price = (3 + 2) * 2;
+console.log("price = " + price);
+
+let addition = 1.1 + 1.3;
+console.log(addition);
+console.log("Number Precision Error");
+
+let u = "Lor'em";
+console.log(u);
+let v = 'Ep"sum';
+console.log(v);
+let w = "Lor\\em";
+console.log(w);
+let x = "Ep\nsum";
+console.log(x);
+let y = "Hell'o' w'o'rld!";
+console.log(y);
+
+let z = `Hello ${v}`;
+console.log(z);
