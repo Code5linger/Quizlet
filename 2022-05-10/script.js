4f741cafe444892924dde6568b9f7eb8981d620c
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-05-10 20:15:42 +0600

    Quizlet

---
+++ 2022-05-10/script.js
@@ -0,0 +1,28 @@
+console.log("Test");
+
+//Single Line Command
+
+/*Multi
+  Line 
+  Command 
+*/
+
+let test = 7;
+console.log(test);
+
+let string = "Test";
+console.log(string);
+
+let num = 17;
+console.log(num);
+
+let bool = true;
+console.log(bool);
+
+let a = 22,
+  b = "Copa",
+  c = false;
+console.log(a, b, c);
+
+let empty;
+console.log(empty);
