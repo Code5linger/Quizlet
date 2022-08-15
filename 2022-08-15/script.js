e17c0f3d6bb1b9b7796ba3d8d179520fc2539361
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-08-15 18:05:22 +0600

    Q2

--- 2022-08-15/script.js
+++ 2022-08-15/script.js
@@ -1,1 +1,33 @@
 console.log("External JS");
+/*
+let testNum = 17;
+let testString = "Testing";
+let bool = "False";
+
+console.log(testNum, testString, bool);
+*/
+
+var testNum = 15,
+  testString = "AD",
+  bool = "True";
+
+console.log("testNum == " + testNum, testString, bool);
+
+let empty;
+
+console.log(empty);
+
+var testNum = 55;
+console.log("testNum == " + testNum);
+
+empty = 69;
+console.log(empty);
+
+const pi = "3.1416";
+//pi = "4.1416";
+
+//const mt;
+
+let error = "Boom💥";
+
+console.log(error);
