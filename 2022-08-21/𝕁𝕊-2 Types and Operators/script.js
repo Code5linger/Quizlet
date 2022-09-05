c9bddfe0d2445e8c51f2da17dbb006f4a298ad70
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-09-06 03:25:58 +0600

    W

--- 2022-08-21/𝕁𝕊-2 Types and Operators/script.js
+++ 2022-08-21/𝕁𝕊-2 Types and Operators/script.js
@@ -4,8 +4,32 @@ let stringX = "Test";
 console.log(typeof num);
 console.log(typeof stringX);
 */
-
+/*
 let num = 7;
 --num;
 
 console.log(num);
+*/
+/*
+let price = (3 + 2) * 2;
+
+console.log(price);
+*/
+/*
+let sum = 1.1 + 1.3;
+
+console.log(sum);
+*/
+/*
+let test = `Copa`;
+let string = 'lor"e${test}`m \\ip"\nsum';
+
+console.log(string);
+console.log(test);
+*/
+
+let fname = "CodeSlinger";
+
+let message = `Hello! ${fname}`;
+
+console.log(message);
