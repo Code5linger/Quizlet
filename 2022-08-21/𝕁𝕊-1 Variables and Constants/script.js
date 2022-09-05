f6a6620093a1e0dcd3c38cc2e2d004a611b90775
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-09-06 00:49:48 +0600

    𝕁𝕊-1 Variables and Constants

--- 2022-08-21/𝕁𝕊-1 Variables and Constants/script.js
+++ 2022-08-21/𝕁𝕊-1 Variables and Constants/script.js
@@ -1,4 +1,19 @@
+/*
 let test = 17,
   string = "Test",
   bool = true;
+
 console.log(test, string, bool);
+*/
+/*
+let noVal;
+noVal = 20;
+console.log(noVal);
+*/
+/*
+const test ;
+console.log(test);
+*/
+console.log(test);
+
+var test = 17;
